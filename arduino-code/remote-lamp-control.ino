b39e285bdece120fdfc22e6b750f33181f96a070
Graziee6 <gngrace10@gmail.com>
2023-01-28 19:51:38 +0200

    switch lamp with arduino codes

---
+++ arduino-code/remote-lamp-control.ino
@@ -0,0 +1,35 @@
+#include <ESP8266WiFi.h>
+int RELAY_PIN = 4;
+int SENSOR_PIN = 12;
+String status="OFF";
+
+void setup() {
+  pinMode(RELAY_PIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(RELAY_PIN, LOW);
+  checkBrightness();
+  delay(1000);
+  
+  digitalWrite(RELAY_PIN, HIGH);
+  checkBrightness();
+  delay(1000);
+}
+
+void checkBrightness(){
+  int brightnessValue= analogRead(SENSOR_PIN);
+  if (brightnessValue < 10) {
+    Serial.println("OFF");
+    status="OFF";
+  } else if (brightnessValue>10) {
+    Serial.println("ON");
+    status="ON";
+  } else {
+    Serial.println("The sensor got us no value");
+  }
+  Serial.print("Analog reading: ");
+  Serial.println(brightnessValue);
+  delay(10000);
+ }
